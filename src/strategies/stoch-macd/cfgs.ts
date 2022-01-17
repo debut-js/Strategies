6479e363db47a5b37bf2620c2adf093b22521597
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2022-01-17 17:01:44 +0300

    fix stoch macd

--- src/strategies/stoch-macd/cfgs.ts
+++ src/strategies/stoch-macd/cfgs.ts
@@ -1,5 +1,26 @@
 import { StochMacdOptions } from './bot';
 
+export const ETHUSDT: StochMacdOptions = {
+    instrumentType: 'SPOT',
+    fee: 0.0003,
+    lotsMultiplier: 1,
+    equityLevel: 1,
+    broker: 'binance',
+    ticker: 'ETHUSDT',
+    currency: 'USDT',
+    interval: '15min',
+    amount: 500,
+    stopLoss: 3.78,
+    takeProfit: 3.87,
+    periodEmaFast: 14,
+    periodEmaSlow: 16,
+    periodSignal: 5,
+    stochPeriod: 25,
+    stochSmaPeriod: 8,
+    signalLife: 1,
+    id: 4,
+};
+
 export const TSLA: StochMacdOptions = {
     broker: 'alpaca',
     ticker: 'TSLA',
