0818283b8e4e50aecc21a685b899d3fa9aeb00cc
Dmitriy Yurov <dyurov@tradingview.com>
2022-07-27 09:25:39 +0300

    log all accounts list

--- src/tools/getAccountId.ts
+++ src/tools/getAccountId.ts
@@ -2,6 +2,6 @@ import { TinkoffTransport } from '@debut/community-core';
 import { cli } from '@debut/plugin-utils';
 const { tinkoff } = cli.getTokens();
 
-new TinkoffTransport(tinkoff, 'asd').api.users.getAccounts({}).then(res => {
-    console.log(res.accounts.find(account => account.type === 1).id);
+new TinkoffTransport(tinkoff, 'asd').api.users.getAccounts({}).then((res) => {
+    console.log(res.accounts);
 });
